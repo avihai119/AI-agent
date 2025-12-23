2f8cdfa4d6ff9c89777a128e896aa0f776edf762
avihai119 <avihi119@gmail.com>
2025-12-23 17:05:23 +0200

    added user token amount and model response amount

--- main.py
+++ main.py
@@ -9,14 +9,22 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
 
     if api_key is None:
-        raise RuntimeError("API key local variable is not found/set!")
+        raise RuntimeError("API key local variable is not found/set")
 
     client = genai.Client(api_key=api_key)
+    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
+        contents=user_prompt  )
     
+    
+    if response.usage_metadata is None:
+        raise RuntimeError("No metadata detected")
+    
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response from model:")
     print(response.text)
 
