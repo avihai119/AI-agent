bb70c2306919e5405330ff994dcf06165528d873
avihai119 <avihi119@gmail.com>
2025-12-28 18:49:48 +0200

    added get_files_info.py and test_get_files.py functions

---
+++ functions/get_files_info.py
@@ -0,0 +1,22 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    abs_pwd_path = os.path.abspath(working_directory)
+    path_to_target_dir = os.path.normpath(os.path.join(abs_pwd_path, directory))
+    valid_target_dir = os.path.commonpath([abs_pwd_path, path_to_target_dir]) == abs_pwd_path
+
+    if not valid_target_dir:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(path_to_target_dir):
+        return f'Error: "{directory}" is not a directory'
+    
+    target_dir_list = os.listdir(path_to_target_dir)
+    target_dir_data = ""
+
+    for item in target_dir_list:
+        full_path = os.path.join(path_to_target_dir, item)
+        is_dir = os.path.isdir(full_path)
+        size = os.path.getsize(full_path)
+        target_dir_data += f"   - {item}: file_size={size} bytes, is_dir={is_dir}\n"
+
+    return target_dir_data
